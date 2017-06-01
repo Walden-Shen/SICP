72bf17f7d54f9f1fb3b486eb3caf83b688ebe41d
Walden-Shen <41880356@qq.com>
2017-06-01 16:23:42 +0800

    before 1.31

--- chapter1/2.scm
+++ chapter1/2.scm
@@ -31,3 +31,63 @@
 		(else (+ (pascal (- x 1) (- y 1)) (pascal x (- y 1))))))
 ;1.13
 ;I have read the prove procudure
+;1.14 theory question
+;1.15
+(define (cube x) (* x x x))
+(define (p x)(- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+ 	(if (< (abs angle) 0.1)
+	 	angle
+		(p (sine (/ angle 3.0)))))
+;1.16
+(define (fast-expt b n) (fast-expt-ancillary b b (- n 1)))
+(define (fast-expt-ancillary a b n)
+ (cond  ((= n 0) a)
+  		((even? n) (fast-expt-ancillary (* a a) b (/ n 2)))
+		(else (fast-expt-ancillary (* a b) b (- n 1)))))
+;1.17 similar to 16
+;1.18 unable to finish since i didn't finish 1.17 :p
+;1.19
+(define (fib n) (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+ (cond  ((= count 0) b)
+  		((even? count) (fib-iter 	a 
+									b 
+								(+ (square p) (square q)) 
+								(+ (square q) (* 2 p q)) 
+								(/ count 2)))
+		(else (fib-iter (+ (* b q) (* a q) (* a p))
+			   			(+ (* b p) (* a q))
+						p
+						q
+						(- count 1)))))
+;1.20 theoretical
+;prime
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+ (cond	((> (square test-divisor) n) n)
+  		((divides? test-divisor n) test-divisor)
+		(else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+(define (prime? n) (= n (smallest-divisor n)))
+;fermat
+(define (expmod base exp m)
+ (cond  ((= exp 0) 1)
+  		((even? exp)
+		 (remainder (square (expmod base (/ exp 2) m)) m))
+		(else (remainder (* base (expmod base (- exp 1) m)) m))))
+(define (fermat-test n)
+ (define (try-it a)
+  (= (expmod a n n) a))
+ (try-it (+ 1 (random (- n 1)))))
+(define (fast-prime? n times)
+ (cond  ((= times 0) #t)
+  		((fermat-test n) (fast-prime? n (- times 1)))
+		(else #f)))
+;1.21 too simple
+;1.22
+(define (timed-prime-test n) (newline) (display n) (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+ (if (prime? n) (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time) (display " *** ") (display elapsed-time))
+
--- chapter1/3.scm
+++ chapter1/3.scm
@@ -1,52 +1,34 @@
-;1.14 theory question
-;1.15
+;abstraction
+(define (sum term a next b)
+ (if (> a b)
+  	 0
+	 (+ (term a) (sum term (next a) next b))))
+(define (inc n) (+ n 1))
 (define (cube x) (* x x x))
-(define (p x)(- (* 3 x) (* 4 (cube x))))
-(define (sine angle)
- 	(if (< (abs angle) 0.1)
-	 	angle
-		(p (sine (/ angle 3.0)))))
-;1.16
-(define (fast-expt b n) (fast-expt-ancillary b b (- n 1)))
-(define (fast-expt-ancillary a b n)
- (cond  ((= n 0) a)
-  		((even? n) (fast-expt-ancillary (* a a) b (/ n 2)))
-		(else (fast-expt-ancillary (* a b) b (- n 1)))))
-;1.17 similar to 16
-;1.18 unable to finish since i didn't finish 1.17 :p
-;1.19
-(define (fib n) (fib-iter 1 0 0 1 n))
-(define (fib-iter a b p q count)
- (cond  ((= count 0) b)
-  		((even? count) (fib-iter 	a 
-									b 
-								(+ (square p) (square q)) 
-								(+ (square q) (* 2 p q)) 
-								(/ count 2)))
-		(else (fib-iter (+ (* b q) (* a q) (* a p))
-			   			(+ (* b p) (* a q))
-						p
-						q
-						(- count 1)))))
-;1.20 theoretical
-;fermat
-(define (expmod base exp m)
- (cond  ((= exp 0) 1)
-  		((even? exp)
-		 (remainder (square (expmod base (/ exp 2) m)) m))
-		(else (remainder (* base (expmod base (- exp 1) m)) m))))
-(define (fermat-test n)
- (define (try-it a)
-  (= (expmod a n n) a))
- (try-it (+ 1 (random (- n 1)))))
-(define (fast-prime? n times)
- (cond  ((= times 0) #t)
-  		((fermat-test n) (fast-prime? n (- times 1)))
-		(else #f)))
-;1.21 too simple
-;1.22
-(define (timed-prime-test n) (newline) (display n) (start-prime-test n (runtime)))
-(define (start-prime-test n start-time)
- (if (fast-prime? n 10) (report-prime (- (runtime) start-time))))
-(define (report-prime elapsed-time) (display " *** ") (display elapsed-time))
+(define (sum-cubes a b) (sum cube a inc b))
+(define (pi-sum a b)
+ (define (pi-term n) (/ 1.0 (* n (+ n 2))))
+ (define (pi-next n) (+ n 4))
+ (sum pi-term a pi-next b))
+;calculus
+(define (integral f a b dx)
+ (define (add-dx x) (+ x dx))
+ (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
+;1.29
+(define (new-sum term a next b count)
+ (define (product count) (if (= (remainder count 2) 0) 2 4))
+ (if (> a b)
+  	 0
+ 	 (+ (* (product count) (term a)) (new-sum term (next a) next b (+ count 1)))))
+(define (simpson f a b n)
+ (define h (/ (- b a) n))
+ (define (simpson-next x) (+ x h))
+ (* (/ h 3.0) (- (new-sum f a simpson-next b 0) (f a) (f b))))
+;1.30
+(define (new-new-sum term a next b)
+ (define (iter a result)
+  (if   (> a b)
+   		result
+		(iter (next a) (+ result (term a))))
+  (iter a 0)))
 
