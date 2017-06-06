374662bb0b79241566330de12ae51c927fd4ff10
Walden-Shen <41880356@qq.com>
2017-06-06 12:01:57 +0800

    before 2.39

--- chapter2/2.scm
+++ chapter2/2.scm
@@ -140,3 +140,32 @@
  (let ((cols (transpose n))) (map (lambda (col-of-m) 
 								   (map (lambda (col-of-cols) (dot-product col-of-m col-of-cols))
 																 cols)) m)))
+;2.38 op should satisfy the rule of exchange if the two fold wanna produce the same result
+(define (fold-left op initial sequence)
+ (define (iter result rest) (if (null? rest) result (iter (op result (car rest)) (cdr rest))))
+ (iter initial sequence))
+;2.39
+(define (reverse sequence) (fold-left (lambda (x y) (cons y x)) '() sequence))
+(define (reverse sequence) (fold-right (lambda (x y) (append y (list x))) '() sequence))
+;nested mappings
+(define (generate-pair n) (accumulate append '() 
+						   (map (lambda (i) 
+								 (map (lambda (j) (list i j))
+								  (enumerate-interval 1 (- i 1))))
+							(enumerate-interval 1 n))))
+(define (prime-sum? pair) (prime? (+ (car pair) (cadr pair))))
+(define (make-pair-sum pair) (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+(define (prime-sum-pairs n) (map make-pair-sum (filter prime-sum? (generate-pair n))))
+;assist the prime? above
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+ (cond	((> (square test-divisor) n) n)
+  		((divides? test-divisor n) test-divisor)
+		(else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+(define (prime? n) (= n (smallest-divisor n)))
+;permutations
+(define (flatmap proc seq) (accumulate append '() (map proc seq)))
+(define (permutations s) (if (null? s) 
+						     (list '()) 
+							 (flatmap (lambda (x) (map (lambda (p) (cons x p)) (permutations (remove x s)))) s)))
