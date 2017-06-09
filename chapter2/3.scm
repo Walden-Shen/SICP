14df299cc2e0c50aaa8fa53fe6674a31e92ee3f1
Walden-Shen <41880356@qq.com>
2017-06-09 12:00:44 +0800

    before 2.72

--- chapter2/3.scm
+++ chapter2/3.scm
@@ -168,9 +168,56 @@
 (define (leaf? object) (eq? (car object) 'leaf))
 (define (symbol-leaf x) (cadr x))
 (define (weight-leaf x) (caddr x))
-(define (make-code-tree left right) (list left right (append (symbol left) (symbol right)) (+ (weight left) (weight right))))
+(define (make-code-tree left right) (list left right (append (symbols left) (symbols right)) (+ (weight left) (weight right))))
 (define (left-branch tree) (car tree))
 (define (right-branch tree) (cadr tree))
 (define (symbols tree) (if (leaf? tree) (list (symbol-leaf tree)) (caddr tree)))
 (define (weight tree) (if (leaf? tree) (weight-leaf tree) (cadddr tree)))
-
+(define (decode bits tree)
+ (define (decode-1 bits current-branch)
+  (if (null? bits) '()
+   (let ((next-branch (choose-branch (car bits) current-branch)))
+	(if (leaf? next-branch) 
+	 (cons (symbol-leaf next-branch) (decode-1 (cdr bits) tree))
+	 (decode-1 (cdr bits) next-branch)))))
+ (decode-1 bits tree))
+(define (choose-branch bit branch)
+ (cond  ((= bit 0) (left-branch branch))
+  		((= bit 1) (right-branch branch))
+		(else (error "bad bit -- CHOOSEBRANCH" bit))))
+(define (adjoin-set x set)
+ (cond  ((null? set) (list x))
+  		((< (weight x) (weight (car set))) (cons x set))
+		(else (cons (car set) (adjoin-set x (cdr set))))))
+(define (make-leaf-set pairs)
+ (if (null? pairs) '() (let ((pair (car pairs))) (adjoin-set (make-leaf (car pair) (cadr pair)) (make-leaf-set (cdr pairs))))))
+;2.67 adabbca
+(define sample-tree 
+ (make-code-tree (make-leaf 'A 4)
+  				 (make-code-tree
+				  (make-leaf 'B 2)
+				  (make-code-tree (make-leaf 'D 1)
+				   				  (make-leaf 'C 1)))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+;2.68
+(define (encode message tree)
+ (if (null? message) '() (append (encode-symbol (car message) tree) (encode (cdr message) tree))))
+(define (in? symbol list)
+ (cond 	((null? list) #f)
+  		((eq? symbol (car list)) #t)
+		(else (in? symbol (cdr list)))))
+(define (encode-symbol symbol tree)
+ (cond  ((and (leaf? tree) (eq? symbol (symbol-leaf tree))) '())
+  		((in? symbol (symbols (left-branch tree))) (cons 0 (encode-symbol symbol (left-branch tree))))
+		((in? symbol (symbols (right-branch tree))) (cons 1 (encode-symbol symbol (right-branch tree))))
+		(else (error "bad tree"))))
+;2.69
+(define (generate-huffman-tree pairs) (successive-merge (make-leaf-set pairs)))
+(define (successive-merge ordered-pairs)
+ (define (anx ordered-pairs previous-tree)
+  (if (null? ordered-pairs) previous-tree 
+   (anx (cdr ordered-pairs) (make-code-tree (car ordered-pairs) previous-tree))))
+ (anx (cdr ordered-pairs) (car ordered-pairs)))
+;2.70 theoretical
+;2.71 1 n-1
+;2.72 n ~ n^2
