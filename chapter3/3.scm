2f20a73d5b42f4ec1824674b0b04f1f4abfcf4ca
Walden-Shen <41880356@qq.com>
2017-06-18 16:52:14 +0800

    before 3.37

--- chapter3/3.scm
+++ chapter3/3.scm
@@ -364,5 +364,36 @@
 (define C (make-connector)) (define F (make-connector)) (celsius-fahrenheit-converter C F)
 (probe "Celsius temp" C) (probe "Fahrenheit temp" F)
 (set-value! C 25 'user)
-(forget-value! C 'use) ;don't work because wrong informant & retractor
-;(set-value! F 212 'use)
+(forget-value! C 'user) ;don't work because wrong informant & retractor
+(set-value! F 212 'user)
+;3.33
+(define (averager a b c)
+ (let ((d (make-connector)) (e (make-connector)))
+  (adder a b d) (constant 0.5 e) (multiplier d e c) 'ok))
+;3.34  when b has been set, a will not get a answer cuz neither a1 and a2 have a value
+(define (squarer a b) (multiplier a a b))
+;3.35
+(define (squarer a b)
+ (define (process-new-value)
+  (if (has-value? b)
+   (if (< (get-value b) 0)
+	(error "square less than 0" (get-value b))
+	(set-value! a (sqrt (get-value b)) me))
+   (set-value! b (square (get-value a)) me)))
+ (define (process-forget-value) (forget-value! a me) (forget-value! b me))
+ (define (me request)
+   (cond ((eq? request 'I-have-a-value) (process-new-value))
+		 ((eq? request 'I-lost-my-value) (process-forget-value))
+		 (else (error "Unknown request --- SQUARER" request))))
+ (connect a me) (connect b me) me)
+;test
+(define a (make-connector)) (define b (make-connector)) (define c (make-connector)) (define d (make-connector))
+(define e (make-connector)) (adder a b c) (multiplier c d e) (probe 'a a) (probe 'b b) (probe 'c c) (probe 'd d) (probe 'e e)
+;3.36 roger
+;3.37
+(define (celsius-fahrenheit-converter x) (c+ (c* (c/ (cv 9) (cv 5)) x) (cv 32)))
+(define (c+ x y) (let ((z (make-connector))) (adder x y z) z))
+(define (c- x y) (let ((z (make-connector))) (adder x z y) z))
+(define (c* x y) (let ((z (make-connector))) (multiplier x y z) z))
+(define (c/ x y) (let ((z (make-connector))) (multiplier x z y) z))
+(define (cv x) (let ((z (make-connector))) (constant x z) z))
