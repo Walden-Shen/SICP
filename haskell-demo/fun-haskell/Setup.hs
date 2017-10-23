caaa1dab05e688621ab2775c12e9a79350b41893
Walden-Shen <41880356@qq.com>
2017-10-23 18:43:12 -0400

    some work on haskell

--- haskell-demo/fun-haskell/Setup.hs
+++ haskell-demo/fun-haskell/Setup.hs
@@ -31,5 +31,113 @@ factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
-addVectors :: (Num a) => (a, a) -> (a, a)
-addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2) 
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+head' :: [a] -> a
+head' [] = error "You r dummy!"
+head' (x:_) = x
+
+length2' :: (Num b) => [a] -> b
+length2' [] = 0
+length2' (_:xs) = 1 + length' xs
+
+sum2' :: (Num a) => [a] -> a
+sum2' [] = 0
+sum2' (x:xs) = x + sum2' xs
+
+capital :: String -> String
+capital "" = "Empty string"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | weight / height ^ 2 <= 18.5 = "u r underweight"
+    | weight / height ^ 2 <= 25.0 = "Welllll"
+    | otherwise                   = "Fatass!"
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b     = a
+    | otherwise = b
+
+bmiTell' :: (RealFloat a) => a -> a -> String
+bmiTell' weight height
+    | bmi <= skinny = "u r underweight"
+    | bmi <= normal = "Welllll"
+    | otherwise                   = "Fatass!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+
+{-or we can write (skinny, normal) = (18.5, 25.0) -}
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+head'' :: [a] -> a
+head'' xs = case xs of [] -> error "no head for empty list"
+                       (x:_) -> x
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:replicate' (n - 1) x
+
+
+replicate'' :: (Num i, Ord i) => i -> a -> [a]
+replicate'' 0 x = []
+replicate'' i x = x:replicate'' (i - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ []   = []
+take' n (x:xs) = x:take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
