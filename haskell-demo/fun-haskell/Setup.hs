f75625346fb2cca64eb8cf0630b471fd0be2f1fb
Walden-Shen <41880356@qq.com>
2017-10-23 12:43:19 -0400

    some work on haskell

--- haskell-demo/fun-haskell/Setup.hs
+++ haskell-demo/fun-haskell/Setup.hs
@@ -11,4 +11,25 @@ boomBang xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
+removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [x | x <- st, x `elem` ['A' .. 'Z']]
+
+rightTriangles = [(a, b, c) | c <- [1 .. 10], b <- [1 .. c], a <- [1 .. b], a^2 + b^2 == c^2]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+{- read "5" + 5 
+   read "5" :: Int -}
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe x = "Bad!"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2) 
