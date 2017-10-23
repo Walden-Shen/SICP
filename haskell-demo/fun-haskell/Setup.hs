394e43df0fcfc7d16e5c1a7b701d63cca3fabe85
Walden-Shen <41880356@qq.com>
2017-10-22 23:15:14 -0400

    some work on haskell

--- haskell-demo/fun-haskell/Setup.hs
+++ haskell-demo/fun-haskell/Setup.hs
@@ -6,3 +6,9 @@ doubleSmallNumber x = (if x > 100 then x else x*2) + 1
 {-[1, 2, 3] ++ [4, 5]
 "hello" ++ "world"
 'a': " small cat"-}
+
+boomBang xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [x | x <- st, x `elem` ['A' .. 'Z']]
